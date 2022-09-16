a89690fcb8e7c9d8db959e0607d8f49cca9b915b
Agung Rilo Pambudi <56600401+AgungRilo@users.noreply.github.com>
2022-09-16 11:02:20 +0700

    REA4A - Agung Rilo Pambudi - Promise + Async & Await

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
-
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(parameter)=>{
+    let hasil = 0
+    let dataTheaterIXX 
+    let dataTheaterVGC 
+    const hitungPromiseOutput = async ()=>{
+      try {
+        dataTheaterIXX = await promiseTheaterIXX()
+        dataTheaterVGC = await promiseTheaterVGC()
+
+        await dataTheaterIXX.forEach(element => {
+          hasil += element.hasil === parameter ? 1 : 0
+        });
+        await dataTheaterVGC.forEach(element => {
+          hasil += element.hasil === parameter ? 1 : 0
+        });
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    await hitungPromiseOutput()
+    return hasil
+};
+
 
 module.exports = {
   promiseOutput,
